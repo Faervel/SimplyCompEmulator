3d37323815566fd431c8c0607a6f87c41438e1ad
Faervel <nickmisharin@gmail.com>
2018-02-13 13:36:27 +0500

    dev main func in Interp class

--- src/com/simplycompemulator/Interpreter.java
+++ src/com/simplycompemulator/Interpreter.java
@@ -9,5 +9,20 @@ public class Interpreter {
     static int data; // Текущий операнд
     static boolean run_bit = true; // Бит сброса машины для остановки
 
+    public static void interpret(int memory[], int starting_address) {
+        PC = starting_address;
+        while(run_bit) {
+            instr = memory[PC];
+            PC += 1;
+            instr_type = get_instr_type(instr);
+            data_loc = find_data(instr, instr_type);
 
+            if (data_loc>=0) data = memory[data_loc];
+            execute(instr_type, data);
+        }
+    }
+
+    private static int get_instr_type(int addr) { return 0;}
+    private static int find_data(int instr, int instr_type) {return 0;}
+    private static void execute(int type, int data) {}
 }
